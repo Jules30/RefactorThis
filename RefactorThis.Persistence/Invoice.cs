847bf365a03da6bf66688e26757ea38b388585fa
julsbartolome <julius-kenneth.bartolome@hill-labs.co.nz>
2025-11-11 21:18:05 +0800

    Simplify Full and Partial methods. - Removed the switch cases. - Created an enum extension to detect if Invoice type is Supported. - Add a readonly private variable to hold tax percentage.

--- RefactorThis.Persistence/Invoice.cs
+++ RefactorThis.Persistence/Invoice.cs
@@ -6,7 +6,8 @@ namespace RefactorThis.Persistence
 {
 	public class Invoice
 	{
-		private readonly InvoiceRepository _repository;
+		private readonly decimal _taxPercentage = 0.14m;
+        private readonly InvoiceRepository _repository;
 		public Invoice(InvoiceRepository repository)
 		{
 			_repository = repository;
@@ -26,51 +27,43 @@ namespace RefactorThis.Persistence
 		public bool InvoiceHasPayments { get => Payments != null && Payments.Any(); }
 		public bool InvoiceIsAlreadyFullyPaid { get => Payments.Sum(x => x.Amount) != 0 && Amount == Payments.Sum(x => x.Amount); }
 		public bool IsPaymentsGreaterThanThePartial(Payment payment) => Payments.Sum(x => x.Amount) != 0 && payment.Amount > (Amount - AmountPaid);
-
-		public string PartialPay(Payment payment) 
+		public string PartialPay(Payment payment)
 		{
-            string responseMessage;
+			if (!InvoiceTypeExtension.IsInvoiceTypeSupported(Type))
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+			bool isPaymentFinalPartial = (Amount - AmountPaid) == payment.Amount;
 
-            bool isPaymentFinalPartial = (Amount - AmountPaid) == payment.Amount;
+			string responseMessage = isPaymentFinalPartial ? "final partial payment received, invoice is now fully paid" : "another partial payment received, still not fully paid";
 
-            AmountPaid += payment.Amount;
-            Payments.Add(payment);
+			if (Type == InvoiceType.Commercial)
+			{
+				TaxAmount += payment.Amount * _taxPercentage;
+			}
 
-            switch (Type)
-            {
-                case InvoiceType.Standard:
-                    responseMessage = isPaymentFinalPartial ? "final partial payment received, invoice is now fully paid" : "another partial payment received, still not fully paid";
-                    break;
-                case InvoiceType.Commercial:
-                    TaxAmount += payment.Amount * 0.14m;
-                    responseMessage = isPaymentFinalPartial ? "final partial payment received, invoice is now fully paid" : "another partial payment received, still not fully paid";
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-            return responseMessage;
-        }
+			AmountPaid += payment.Amount;
+			Payments.Add(payment);
 
-		public string FullPay(Payment payment) {
-            string responseMessage;
+			return responseMessage;
+		}
 
-            bool isFullPayment = Amount == payment.Amount;
+		public string FullPay(Payment payment)
+		{
+            if (!InvoiceTypeExtension.IsInvoiceTypeSupported(Type))
+			{
+				throw new ArgumentOutOfRangeException();
+			}
 
-            AmountPaid = payment.Amount;
-            TaxAmount = payment.Amount * 0.14m;
-            Payments.Add(payment);
+			bool isFullPayment = Amount == payment.Amount;
+			string responseMessage = isFullPayment ? "invoice is now fully paid" : "invoice is now partially paid";
 
-            switch (Type)
-            {
-                case InvoiceType.Standard:
-                case InvoiceType.Commercial:
-                    responseMessage = isFullPayment ?  "invoice is now fully paid" : "invoice is now partially paid";
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-            return responseMessage;
-        }
+			AmountPaid = payment.Amount;
+			TaxAmount = payment.Amount * _taxPercentage;
+			Payments.Add(payment);
+
+			return responseMessage;
+		}
 	}
 
 	public enum InvoiceType
@@ -78,4 +71,13 @@ namespace RefactorThis.Persistence
 		Standard,
 		Commercial
 	}
+
+	public static class InvoiceTypeExtension
+	{
+		public static bool IsInvoiceTypeSupported(this InvoiceType invoiceType)
+		{
+			return invoiceType == InvoiceType.Standard || invoiceType == InvoiceType.Commercial;
+		}
+	}
+	
 }
